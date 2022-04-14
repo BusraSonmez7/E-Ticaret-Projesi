949bd04340c022d7bc98e770e531d5aef52e6937
Büşra S <87311641+BusraSonmez7@users.noreply.github.com>
2022-04-15 02:22:08 +0300

    resim
    resim sorunu giderildi

--- NarEvimPiga/src/components/Loading/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LottieView from 'lottie-react-native';
-
-const Loading = () => {
-    return <LottieView source = {require('../../assets/loading.json')} autoPlay/>;
-}
-
-export default Loading;
